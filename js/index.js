8cf16594fb835f495af4a42a74875ee0fa67d8b6
desiree <desireegarcia@mini.hsd1.nm.comcast.net>
2021-09-28 16:48:30 -0600

    completed code for copy

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Your code goes here
+//mouseover
 //logo-heading
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('mouseover', function (event){
@@ -7,3 +8,27 @@ logoHeading.addEventListener('mouseover', function (event){
         event.target.style.color = ''
     }, 500);
 }, false);
+
+//focus
+//.intro img
+const focusImg = document.querySelector('.intro img');
+focusImg.addEventListener('focus', (event) => {
+    event.target.style.background = 'orange';    
+});
+focusImg.addEventListener('blur', (event) => {
+    event.target.style.background = '';
+})
+
+//.btn
+//resize
+
+//copy
+//p
+const para = document.querySelector('p');
+para.addEventListener('copy', function (event){
+   para.style.backgroundColor = '#ffde9c';
+   setTimeout(function(){
+       event.target.style.backgroundColor = '';
+   }, 2000);
+}, false);
+
