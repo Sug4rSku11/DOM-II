4c62a3144417b3040c4e3277820429a9f80ed5e8
desiree <desireegarcia@mini.hsd1.nm.comcast.net>
2021-09-28 15:09:49 -0600

    completed code for mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+//logo-heading
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', function (event){
+    event.target.style.color = 'orange';
+    setTimeout(function(){
+        event.target.style.color = ''
+    }, 500);
+}, false);
